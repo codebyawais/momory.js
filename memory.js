2d6e1eb8a83138aa355f1aadff6aa106e4f9e8db
WebDevByAwaisRafiq <webdevbyawais@gmail.com>
2024-06-05 13:58:47 +0500

    Add files via upload

---
+++ memory.js
@@ -0,0 +1,31 @@
+// memory
+// two types
+// Stack 
+// Heap
+
+
+// stack: primitive type me use 
+// heap : non primitive    refernce mile ga
+
+let myname ="awais rafiq";
+
+let anothername= myname;
+anothername = "chai our code"
+
+// console.log(anothername);
+// console.log(myname);
+
+let userone={
+    email:"user@gmail.com",
+    upi:"user@ybl"
+}
+
+let usertwo=userone;
+usertwo.email="awais@gmail"
+console.log(userone.email);
+console.log(usertwo.email);
+
+
+
+
+
